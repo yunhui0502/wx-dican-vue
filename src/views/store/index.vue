3ff15c1474c0d2238c8eaba9673de84d8df373f2
‘yunhui0502 <2661234231@qq.com>
2020-07-29 16:10:34 +0800

    1

--- src/views/store/index.vue
+++ src/views/store/index.vue
@@ -11,8 +11,10 @@
     <!-- <el-card v-if="criteria==1" class="box-card"> -->
     <el-card v-if="criteria==1" class="box-card">
       <div style="display:flex;justify-content : flex-end;color: red;font-size: 12px;">*备注：请先下载用户数据，修改后重新上传</div>
-      <div style="display:flex;justify-content : flex-end;margin: 10px;">
-        <el-button size="small" style="margin-right: 10px;" @click="Derived" type="primary">+ 添加用户</el-button>
+      <div style="display:flex;justify-content : space-between;margin: 10px;">
+        <el-button size="small"  @click="Derived" type="primary">下载</el-button>
+        <div style="display:flex;">
+          <el-button size="mini" style="margin-right: 10px;" @click="dialogFormVisible = true" type="primary">+ 添加用户</el-button>
         <el-upload
           class="upload-demo"
           action="api/api/dichan/company"
@@ -28,6 +30,7 @@
         >
           <el-button size="small" type="primary">点击上传</el-button>
         </el-upload>
+        </div>
       </div>
 
       <el-table :data="tableData" border style="width: 100%">
@@ -36,7 +39,6 @@
         <el-table-column align="center" prop="phone" label="手机号"></el-table-column>
         <el-table-column align="center" prop="address6" label="操作">
           <template slot-scope="scope" width="200">
-            <el-button size="mini" @click="dialogFormVisible = true" type="primary">添加用户</el-button>
             <el-button size="mini" @click="Delete(scope)" type="danger">删除</el-button>
           </template>
         </el-table-column>
@@ -72,12 +74,13 @@
     <!-- --------------------------------回收站-------------------------------------- -->
 
     <el-card v-if="criteria==2" class="box-card">
+      <!-- <div style="display:flex;justify-content : flex-end;color: red;font-size: 12px;">111</div> -->
       <div style="display:flex;justify-content : space-between;margin: 10px;">
         <el-button size="small" @click="dialogFormSetting = true" type="primary">+ 添加管理员</el-button>
       </div>
 
-      <el-table :data="tableData2" border style="width: 100%">
-        <el-table-column align="center" prop="id" label="ID"></el-table-column>
+   <el-table :data="tableData2" border style="width: 100%">
+        <!-- <el-table-column align="center" prop="id" label="ID"></el-table-column> -->
         <el-table-column align="center" prop="name" label="用户名称"></el-table-column>
         <el-table-column align="center" prop="phone" label="手机号"></el-table-column>
         <el-table-column align="center" prop="address6" label="操作">
@@ -86,6 +89,7 @@
           </template>
         </el-table-column>
       </el-table>
+
     </el-card>
 
     <el-dialog title="添加管理员" :visible.sync="dialogFormSetting">
